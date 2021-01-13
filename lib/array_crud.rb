0e59973cf7dc3628a6243046808378e38c414476
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-13 19:11:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,50 @@
+
 def create_an_empty_array
-  
+[]
 end
 
 def create_an_array
-  
+best_friends = ["Nat","Em","V","Catie"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["I", "am", "really", "learning"]
+  element = "wow"
 end
+
 
 def add_element_to_start_of_array(array, element)
-  
+  best_friend = ["Natalee", "Catie", "Emily", "Venecia"]
+  best_friend.unshift("Delilah")
 end
+
 
 def remove_element_from_end_of_array(array)
-  
-end
+  best_friend = ["Natalee", "Catie", "Venecia", "Emily"]
+  emily_friend = best_friend.pop
+  end
 
 def remove_element_from_start_of_array(array)
-  
+  best_friend = ["Natalee", "Catie", "Venecia", "Emily"]
+  best_friend.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  best_friend = ["Natalee", "Catie", "Venecia", "Emily"]
+  best_friend[1]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(array,)
+best_friend = ["Natalee", "Catie", "Venecia", "Emily"]
+best_friend[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  best_friend = ["Natalee", "Catie", "Venecia", "Emily"]
+  best_friend[3]
 end
 
-def update_element_from_index(array, index_number, element)
-
+def update_element_from_index(array, index_number, argument)
+  best_friend = ["Natalee","Catie","Venecia","Emily"]
+  best_friend[3]= "Delilah"
 end
---
+++ lib/practice.rb
@@ -0,0 +1,6 @@
+def add_element_to_start_of_array(array, element)
+   array = "#{["Natalee","Catie","Emily","Venecia"]}"
+  array.unshift"#{"Delilah"}
+
+end
+add_element_to_start_of_array
